28fbd4c1caa29986ac4768c66894bdd64e629cc6
wim-vandijck-sp <wim.vandijck@sailpoint.com>
2022-01-06 15:02:28 -0500

    pom.xml

--- src/main/java/sailpoint/ets/EtsApplication.java
+++ src/main/java/sailpoint/ets/EtsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestServiceApplication {
+public class EtsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestServiceApplication.class, args);
+        SpringApplication.run(EtsApplication.class, args);
     }
 
 }
